7dc10f9d8ffe6ce94873114a988e908bcfa87ad5
@nimmet <nimetullah@live.com>
2022-10-06 14:35:11 +0300

    use button to show loaded message

--- ajax.js
+++ ajax.js
@@ -1,21 +1,23 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET',"./ajaxTxt.txt")
-// xhr.onreadystatechange = function() {
-// console.log(xhr);
-// if(xhr.readyState === 4 && xhr.status === 200){
-//     console.log(xhr.responseText);
-// } else {
-//     console.log({
-//         status: xhr.status,
-//         text: xhr.statusText
-//     });
-// }
-// }
-xhr.send()
-xhr.addEventListener('load',()=>{
-    console.log(xhr.responseText);
-    const text = document.createElement('p')
-    text.textContent = xhr.responseText
-    document.body.appendChild(text)
 
-})
+
+        const btn = document.querySelector('.btn')
+
+
+btn.addEventListener('click',()=>{
+    getData()})
+
+function getData(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET',"./ajaxTxt.txt")
+   
+    xhr.send()
+    
+    
+    xhr.addEventListener('load',()=>{
+        const text = document.createElement('p')
+        document.body.appendChild(text)
+        text.textContent = xhr.responseText
+        
+        // console.log(xhr.responseText);
+    })
+}
