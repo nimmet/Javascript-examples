052226d23e6a9f659da795400459915bb24d8c04
@nimmet <nimetullah@live.com>
2022-10-06 14:37:46 +0300

    use button to show loaded message

--- ajax.js
+++ ajax.js
@@ -1,7 +1,8 @@
 
 
         const btn = document.querySelector('.btn')
-
+        const text = document.createElement('p')
+        document.body.appendChild(text)
 
 btn.addEventListener('click',()=>{
     getData()})
@@ -14,8 +15,7 @@ function getData(){
     
     
     xhr.addEventListener('load',()=>{
-        const text = document.createElement('p')
-        document.body.appendChild(text)
+     
         text.textContent = xhr.responseText
         
         // console.log(xhr.responseText);
